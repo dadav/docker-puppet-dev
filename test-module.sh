990ae52976c02a2c9b5c2d5832f5a6de2dd4e298
dadav <33197631+dadav@users.noreply.github.com>
2019-06-20 12:01:59 +0200

    comments

--- test-module.sh
+++ test-module.sh
@@ -5,11 +5,13 @@ SCRIPTPATH="$(dirname "$0")"
 for metadata in "$SCRIPTPATH"/../modules/*/metadata.json; do
   NAME="$(<"$metadata" jq -r ".dependencies[].name")"
   VERSION="$(<"$metadata" jq -r ".dependencies[].version_requirement")"
-  # strip when "<" requirement
-  VERSION="${VERSION%<*}"
-  # remove whitespace
-  VERSION="${VERSION// /}"
+  # strip
+  VERSION="${VERSION%<*}" # < sign
+  VERSION="${VERSION//>/}" # > sign
+  VERSION="${VERSION//=/}" # equal sign
+  VERSION="${VERSION// /}" # whitespace
+
   NAME="${NAME//\//-}"
-  /opt/puppetlabs/bin/puppet module install --debug --modulepath "$SCRIPTPATH"/../modules/ "$NAME"
+  /opt/puppetlabs/bin/puppet module install --debug --modulepath "$SCRIPTPATH"/../modules/ --version "$VERSION" "$NAME"
 done
 /opt/puppetlabs/bin/puppet apply --debug --modulepath "$SCRIPTPATH"/../modules/ "$SCRIPTPATH"/../site.pp
